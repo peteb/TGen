74d1b0cf3a86eadfa240c88f6fc6680154c34a85
peteb <ptr.bckmn@gmail.com>
2007-06-25 01:19:09 +0000

    git-svn-id: svn://epb.mine.nu/TGen/trunk@20 a7b44492-6732-0410-9493-8c8356ca24be

---
+++ Source/mesh.cpp
@@ -0,0 +1,11 @@
+/*
+ *  mesh.cpp
+ *  tgen
+ *
+ *  Created by Peter Backman on 6/25/07.
+ *  Copyright 2007 Peter Backman. All rights reserved.
+ *
+ */
+
+#include "mesh.h"
+
---
+++ Source/mesh.h
@@ -0,0 +1,21 @@
+/*
+ *  mesh.h
+ *  tgen
+ *
+ *  Created by Peter Backman on 6/25/07.
+ *  Copyright 2007 Peter Backman. All rights reserved.
+ *
+ */
+
+#ifndef _TGEN_MESH_H
+#define _TGEN_MESH_H
+
+namespace TGen {
+	class Mesh {
+	public:	
+		
+	};
+	
+} // !TGen
+
+#endif // !_TGEN_MESH_H
