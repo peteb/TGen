f7c21bb307053e24300e5b384a2685ebad251f17
peteb <ptr.bckmn@gmail.com>
2008-06-04 17:08:35 +0000

    git-svn-id: svn://epb.mine.nu/TGen/trunk@360 a7b44492-6732-0410-9493-8c8356ca24be

---
+++ Renderer/Include/vertexoffsetter.h
@@ -0,0 +1,30 @@
+/*
+ *  vertexoffsetter.h
+ *  TGen Renderer
+ *
+ *  Created by Peter Backman on 5/6/08.
+ *  Copyright 2008 Peter Backman. All rights reserved.
+ *
+ */
+
+#ifndef _TGEN_RENDERER_VERTEXOFFSETTER_H
+#define _TGEN_RENDERER_VERTEXOFFSETTER_H
+
+#include "vertextransformer.h"
+#include <tgen_math.h>
+
+namespace TGen {
+	class VertexOffsetter : public TGen::VertexTransformer {
+	public:
+		VertexOffsetter(const TGen::Vector3 & offset);
+		
+		void transform(TGen::Vector3 & vector) const;
+		TGen::VertexOffsetter * clone() const;
+		
+	private:
+		TGen::Vector3 offset;
+	};
+} // !TGen
+
+#endif // !_TGEN_RENDERER_VERTEXOFFSETTER_H
+
---
+++ Renderer/Source/vertexoffsetter.cpp
@@ -0,0 +1,24 @@
+/*
+ *  vertexoffsetter.cpp
+ *  TGen Renderer
+ *
+ *  Created by Peter Backman on 5/6/08.
+ *  Copyright 2008 Peter Backman. All rights reserved.
+ *
+ */
+
+#include "vertexoffsetter.h"
+#include <tgen_math.h>
+
+TGen::VertexOffsetter::VertexOffsetter(const TGen::Vector3 & offset) 
+	: offset(offset)
+{
+}
+
+void TGen::VertexOffsetter::transform(TGen::Vector3 & vector) const {
+	vector += offset;
+}
+
+TGen::VertexOffsetter * TGen::VertexOffsetter::clone() const {
+	return new TGen::VertexOffsetter(offset);
+}
